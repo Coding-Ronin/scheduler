6ccb9fbf3a202cb1849b58ff5fd81efa5f6e5b73
ChipuChipu <joshuay.yi@hotmail.com>
2018-11-08 13:36:42 -0800

    Scheduler Prototype
    Proof of Concept

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# scheduler
+Code-Ronins MEAN Calendar Prototype
 
-Scheduler Web Application!
+Description:
+	This is a prototype for the Code Ronins Calendar Web Service Project.
+	At the base, this branch serves as a Proof Of Concept.
+
+Installation:
+	Requirements:
+		- Node.js with npm
+		
+	To Install This: 
+		- Clone Repo
+		- Open Command Prompt and change the directory to the project directory.
+		- Type "npm install"
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+/* Basic Implementation used for Test - Master will use Aaron's Code */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Co - Scheduler' });
+  res.render('index');
+});
+
+//Partial views
+router.get('/partials/:name', function(req, res){
+	var name = req.params.name;
+	res.render('partials/' + name );
+});
+
+//All else redirect to home
+router.get('*', function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
